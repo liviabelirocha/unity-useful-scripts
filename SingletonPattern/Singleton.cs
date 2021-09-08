8ec8f6c2635a47ad10a1b87a4f06b05f23c4112f
liviabelirocha <liviabelirocha@outlook.com>
2021-09-08 10:09:42 -0300

    Singleton Pattern

---
+++ SingletonPattern/Singleton.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class Singleton : MonoBehaviour {
+    void Awake() 
+    {
+        MakeSingleton();
+    }
+
+    private void MakeSingleton() 
+    {
+        if (instance) 
+            Destroy(gameObject);
+        else 
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+}
